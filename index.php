d62b028f2a7064ea470e1444e65e60ecf5afa1da
Saad <shaikhsaad3092003@gmail.com>
2023-04-14 15:49:47 +0530

    Stepup of lang

---
+++ index.php
@@ -0,0 +1,214 @@
+<?php require_once __DIR__."/header.php"?>
+
+<div class="content container-fluid">
+    <div class="page-header">
+        <div class="row align-items-center">
+            <div class="col">
+                <h1 class="page-header-title">Dashboard</h1>
+            </div>
+        </div>
+    </div>
+    <div class="row g-3">
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="card">
+                <div class="card-header bg-light text-primary py-2">
+                    <div class="card-title d-flex justify-content-between align-items-center">
+                        <span class="fs-3">Revenue</span>
+                        <span>
+                            <i class="bi-cash-stack btn-icon icon-circle icon-soft-primary bg-gradient fs-3"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="card-body py-2">
+                    <ul class="list-group list-group-flush card-text">
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Total Revenue</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Yearly Revenue</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Monthly Revenue</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Daily Revenue</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="card">
+                <div class="card-header bg-light text-primary py-2">
+                    <div class="card-title d-flex justify-content-between align-items-center">
+                        <span class="fs-3">Sales</span>
+                        <span>
+                            <i class="bi-cash-coin btn-icon icon-circle icon-soft-primary bg-gradient fs-3"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="card-body py-2">
+                    <ul class="list-group list-group-flush card-text">
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Total Sales</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Yearly Sales</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Monthly Sales</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Daily Sales</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="card">
+                <div class="card-header bg-light text-primary py-2">
+                    <div class="card-title d-flex justify-content-between align-items-center">
+                        <span class="fs-3">Purchase</span>
+                        <span>
+                            <i class="bi-cart-plus btn-icon icon-circle icon-soft-primary bg-gradient fs-3"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="card-body py-2">
+                    <ul class="list-group list-group-flush card-text">
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Total Purchase</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Yearly Purchase</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Monthly Purchase</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Daily Purchase</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="card">
+                <div class="card-header bg-light text-primary py-2">
+                    <div class="card-title d-flex justify-content-between align-items-center">
+                        <span class="fs-3">Expenses</span>
+                        <span>
+                            <i class="bi-currency-exchange btn-icon icon-circle icon-soft-primary bg-gradient fs-3"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="card-body py-2">
+                    <ul class="list-group list-group-flush card-text">
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Total Expenses</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Yearly Expenses</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Monthly Expenses</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Daily Expenses</span>
+                                <span class="rupee-after">0.00</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="card">
+                <div class="card-header bg-light text-primary py-2">
+                    <div class="card-title d-flex justify-content-between align-items-center">
+                        <span class="fs-3">Bookings</span>
+                        <span>
+                            <i class="bi-tags btn-icon icon-circle icon-soft-primary bg-gradient fs-3"></i>
+                        </span>
+                    </div>
+                </div>
+                                <div class="card-body py-2">
+                    <ul class="list-group list-group-flush card-text">
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Total Bookings</span>
+                                <span>0 <small>(0 Plants)</small></span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Yearly Bookings</span>
+                                <span>0 <small>(0 Plants)</small></span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Monthly Bookings</span>
+                                <span>0 <small>(0 Plants)</small></span>
+                            </div>
+                        </li>
+                        <li class="list-group-item px-0 py-2">
+                            <div class="d-flex justify-content-between">
+                                <span>Daily Bookings</span>
+                                <span>0 <small>(0 Plants)</small></span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require_once __DIR__.'/footer.php' ?>
